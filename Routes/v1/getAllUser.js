0acd3053e2cc8e8ef7e5b3c8851cd2fa3a359005
GMShimon <simon.rosedale99@gmail.com>
2023-03-09 08:29:12 +0800

    retrieve all users by limit done'

--- Routes/v1/getAllUser.js
+++ Routes/v1/getAllUser.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get("/all",async(req,res)=>{
-    
+    const {limit} = req.query
+    console.log(limit)
+    fs.readFile('users.json',(err,data)=>{
+        if(err){
+            res.send("Error");
+        }else{
+            res.send(JSON.parse(data).slice(0,limit));
+        }
+    })
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const randomUserRoute = require('./Routes/v1/getRandomUser');
+const getAll = require('./Routes/v1/getAllUser');
 const app = express();
 app.use(express.json())
 
 const PORT = 5000;
 
 app.use('/user',randomUserRoute);
+app.use('/user',getAll);
 
 app.get("/",async(req,res)=>{
     res.send("Hello World!!!")
