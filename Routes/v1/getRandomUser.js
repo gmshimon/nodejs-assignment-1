aa5c5ccefbc34035fca0a44b7f278c1074bf0856
GMShimon <simon.rosedale99@gmail.com>
2023-03-09 08:17:59 +0800

    get random user complete

--- Routes/v1/getRandomUser.js
+++ Routes/v1/getRandomUser.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/random',async (req,res)=>{
-    res.send("Dibo na user der");
+    fs.readFile('users.json',(err,data)=>{
+        if(err){
+            res.send("Error");
+        }else{
+            //get the length of data array
+            const dataLength = JSON.parse(data).length
+            //generate a random number
+            let x = Math.floor((Math.random() * dataLength));
+            res.send(JSON.parse(data)[x]);
+        }
+    })
 })
 
 module.exports = router;
